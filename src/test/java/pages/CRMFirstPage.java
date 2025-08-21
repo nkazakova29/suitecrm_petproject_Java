d7c8c65bd287c140e814be464278c59bf39b21e4
Nadezhda Kazakova <nkazakova2992@gmail.com>
2025-08-21 13:51:57 -0700

    Allure steps

--- src/test/java/pages/CRMFirstPage.java
+++ src/test/java/pages/CRMFirstPage.java
@@ -4,47 +4,51 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.PlaywrightException;
 import com.microsoft.playwright.options.WaitForSelectorState;
+import io.qameta.allure.Step;
 
 public class CRMFirstPage {
 
-        private final Page page;
-        private final String url = "https://suitecrm.com/demo/";
-        private final Locator goToLogInBtn;
-        private final Locator loginField;
-        private final Locator passwordField;
-        private final Locator submitBtn;
+    private final Page page;
+    private final String url = "https://suitecrm.com/demo/";
+    private final Locator goToLogInBtn;
+    private final Locator loginField;
+    private final Locator passwordField;
+    private final Locator submitBtn;
 
 
-        public CRMFirstPage(Page page) {
-            this.page = page;
-            this.goToLogInBtn = page.locator("text=ACCESS THE SUITECRM 7 ESR DEMO");
-            this.loginField = page.locator("#user_name");
-            this.passwordField = page.locator("#username_password");
-            this.submitBtn = page.locator("#bigbutton");
+    public CRMFirstPage(Page page) {
+        this.page = page;
+        this.goToLogInBtn = page.locator("text=ACCESS THE SUITECRM 7 ESR DEMO");
+        this.loginField = page.locator("#user_name");
+        this.passwordField = page.locator("#username_password");
+        this.submitBtn = page.locator("#bigbutton");
+    }
+
+    @Step("Open the target site")
+    public void open() {
+        page.navigate(url);
+    }
+
+    @Step("Go to the Login Page")
+    public void goTOLogInPage(){
+        goToLogInBtn.click();
+    }
+
+    @Step("Login process")
+    public void login(String username, String password) {
+        loginField.fill(username);
+        passwordField.fill(password);
+        submitBtn.click();
+    }
+    @Step("Checking that user can proceed to the next page")
+    public boolean isLoggedIn() {
+        // пример проверки: ищем элемент с id="user-name"
+        try {
+            page.locator("text=Welcome to the SuiteCRM 7 Demo")
+                    .waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE).setTimeout(5000));
+            return true;
+        } catch (PlaywrightException e) {
+            return false;
         }
-
-        public void open() {
-            page.navigate(url);
-        }
-
-        public void goTOLogInPage(){
-            goToLogInBtn.click();
-        }
-
-        public void login(String username, String password) {
-            loginField.fill(username);
-            passwordField.fill(password);
-            submitBtn.click();
-        }
-
-        public boolean isLoggedIn() {
-            // пример проверки: ищем элемент с id="user-name"
-            try {
-                page.locator("text=Welcome to the SuiteCRM 7 Demo")
-                        .waitFor(new Locator.WaitForOptions().setState(WaitForSelectorState.VISIBLE).setTimeout(5000));
-                return true;
-            } catch (PlaywrightException e) {
-                return false;
-            }
-        }
+    }
 }
--- src/test/java/tests/CRMTests.java
+++ src/test/java/tests/CRMTests.java
@@ -2,12 +2,15 @@ package tests;
 
 import BaseTest.BaseTest;
 import com.microsoft.playwright.*;
+import io.qameta.allure.Description;
 import org.junit.jupiter.api.*;
 import pages.CRMFirstPage;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class CRMTests extends BaseTest {
     @Test
+    @Description("Successful login")
+
     public void testLogin() {
         CRMFirstPage firstPage = new CRMFirstPage(page);
         firstPage.open();

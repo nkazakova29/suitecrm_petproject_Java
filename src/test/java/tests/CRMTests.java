ec9c899b3d7f3e26c1b4ba857201e9850b687b58
Nadezhda Kazakova <nkazakova2992@gmail.com>
2025-08-22 13:38:42 -0700

    allure steps

--- src/test/java/tests/CRMTests.java
+++ src/test/java/tests/CRMTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import BaseTest.BaseTest;
 import com.microsoft.playwright.*;
+import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.*;
 import pages.CRMFirstPage;
@@ -13,9 +14,13 @@ public class CRMTests extends BaseTest {
 
     public void testLogin() {
         CRMFirstPage firstPage = new CRMFirstPage(page);
-        firstPage.open();
+/*        firstPage.open();
         firstPage.goTOLogInPage();
         firstPage.login("will", "will"); // Demo creds
+       ;*/
+        Allure.step("Open the target site", firstPage::open);
+        Allure.step("Go to the Login Page", firstPage::goTOLogInPage);
+        Allure.step("Perform login", () -> firstPage.login("will", "will"));
         assertTrue(firstPage.isLoggedIn(), "Welcome to the SuiteCRM 7 Demo");
     }
 
@@ -23,9 +28,12 @@ public class CRMTests extends BaseTest {
     @Description("Unsuccessful login")
     public void testFailedLogin() {
         CRMFirstPage firstPage = new CRMFirstPage(page);
-        firstPage.open();
+       /* firstPage.open();
         firstPage.goTOLogInPage();
-        firstPage.login("login", "will"); // Demo creds
+        firstPage.login("login", "will"); // Demo creds*/
+        Allure.step("Open the target site", firstPage::open);
+        Allure.step("Go to the Login Page", firstPage::goTOLogInPage);
+        Allure.step("Perform login", () -> firstPage.login("login", "will"));
         assertFalse(firstPage.isLoggedIn(), "Welcome to the SuiteCRM 7 Demo");
         assertTrue(firstPage.isErrorAppear(), "You must specify a valid username and password.");
     }

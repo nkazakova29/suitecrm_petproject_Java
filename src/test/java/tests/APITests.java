7423e28a34e0af6468d16feccdf15ed825f3f50a
nadezhdakazakova <nkazakova2992@gmail.com>
2026-08-12 23:48:41 +0400

    changed structure and added a new test

--- src/test/java/tests/APITests.java
+++ src/test/java/tests/APITests.java
@@ -1,4 +1,30 @@
 package tests;
 
+import BaseTest.BaseTest;
+import com.microsoft.playwright.BrowserContext;
+import data.LoginData;
+import org.junit.jupiter.api.Test;
+import pages.CRMFirstPage;
+import pages.HomePage;
+
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class APITests {
+    public static class AuthSetUpTest extends BaseTest {
+
+    @Test
+        public void authSetUp() {
+            LoginData.LoginCredentials credentials = LoginData.validUser();
+            CRMFirstPage firstPage = new CRMFirstPage(page, baseUrl);
+            HomePage hpage = new HomePage(page);
+            firstPage.open();
+            firstPage.login(credentials.username(), credentials.password());
+            assertTrue(hpage.isLoggedIn(), "Logout");
+            context.storageState(
+                    new BrowserContext.StorageStateOptions()
+                            .setPath(Paths.get("storage/storageState.json")));
+        }
+            }
 }
